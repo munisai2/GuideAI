4c32b3f3de8ebad511046774f9362a78bacce7ca
amithreddy258 <20bds006@iiitdwd.ac.in>
2023-04-30 18:24:21 +0530

    updated categories widget

--- App/lib/home/widgets/categories_widget.dart
+++ App/lib/home/widgets/categories_widget.dart
@@ -3,75 +3,44 @@ import 'package:travel_app_flutter/home/model/home_models.dart';
 
 class CategoriesWidget extends StatelessWidget {
   static const items = [
-    HomeScreenCategoryItems(
-        name: "Restaurents",
-        image: Icon(
-          Icons.restaurant,
-          color: Color(0xFFFE7245),
-        )),
-    HomeScreenCategoryItems(
-        name: "Security",
-        image: Icon(Icons.security, color: Color(0xFFFE7245))),
-    HomeScreenCategoryItems(
-        name: "Live Events",
-        image: Icon(Icons.library_music, color: Color(0xFFFE7245))),
-    HomeScreenCategoryItems(
-        name: "Food", image: Icon(Icons.food_bank, color: Color(0xFFFE7245))),
-    HomeScreenCategoryItems(
-        name: "Lang Trans",
-        image: Icon(Icons.language_rounded, color: Color(0xFFFE7245))),
+    HomeScreenCategoryItems(name: "Restaurants", image: Icon(Icons.restaurant, color: Color(0xFFFE7245))),
+    HomeScreenCategoryItems(name: "Security", image: Icon(Icons.security, color: Color(0xFFFE7245))),
+    HomeScreenCategoryItems(name: "Live Events", image: Icon(Icons.library_music, color: Color(0xFFFE7245))),
+    HomeScreenCategoryItems(name: "Food", image: Icon(Icons.food_bank, color: Color(0xFFFE7245))),
+    HomeScreenCategoryItems(name: "Lang Trans", image: Icon(Icons.language_rounded, color: Color(0xFFFE7245))),
   ];
 
-  Widget _returnItem(HomeScreenCategoryItems item) {
-    return Column(children: [
-      Row(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 60,
-                height: 60,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Center(
-                  child: item.image,
-                ),
-              ),
-              Text(
-                item.name,
-                style: const TextStyle(fontSize: 12, color: Colors.white),
-              )
-            ],
-          ),
-          const SizedBox(
-            width: 12,
-          ),
-          Container()
-        ],
-      ),
-      // SizedBox(
-      //   width: 20,
-      //   height: 20,
-      //   child: DecoratedBox(
-      //     decoration: BoxDecoration(color: Colors.red),
-      //   ),
-      // )
-    ]);
-  }
-
-  const CategoriesWidget({Key? key}) : super(key: key);
-
-  @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
-      child: Row(children: [
-        for (var i = 0; i < items.length; i++) _returnItem(items[i])
-      ]),
+      child: Row(
+        children: items.map((item) => _returnItem(item)).toList(),
+      ),
+    );
+  }
+
+  Widget _returnItem(HomeScreenCategoryItems item) {
+    return Column(
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Center(child: item.image),
+            ),
+            Text(item.name, style: const TextStyle(fontSize: 12, color: Colors.white)),
+          ],
+        ),
+        const SizedBox(width: 12),
+        Container(),
+      ],
     );
   }
 }
--- App/lib/main.dart
+++ App/lib/main.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
+
 import 'home/home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch().copyWith(
-          secondary: const Color(0xFFFE7245), // Accent color
-          primary: const Color(0xFFFCAE40), // Primary color
+            secondary: Color(0xFFFE7245), // Your accent color
+            primary: Color(0xFFFCAE40)
         ),
       ),
       home: HomePage(),
     );
   }
 }
+
